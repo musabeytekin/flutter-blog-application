b69ed1290d40ce1e3130a3e75f0f2b4176bced9d
musabeytekin <02190201003@ogr.inonu.edu.tr>
2024-03-14 01:36:25 +0300

    some refactoring

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,18 +2,23 @@ import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
-    borderSide: BorderSide(
-      color: color,
-      width: 3,
-    ),
-    borderRadius: BorderRadius.circular(10),
-  );
+  static _border([
+    Color color = AppPallete.borderColor,
+  ]) =>
+      OutlineInputBorder(
+        borderSide: BorderSide(
+          color: color,
+          width: 2.4,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+
   static final darkThemeMode = ThemeData.dark().copyWith(
-      scaffoldBackgroundColor: AppPallete.backgroundColor,
-      inputDecorationTheme: InputDecorationTheme(
-        contentPadding: EdgeInsets.all(27),
-        enabledBorder: _border(),
-        focusedBorder: _border(AppPallete.gradient2),
-      ));
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
+    ),
+  );
 }
